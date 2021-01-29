4ceb07aba335985e858abb81d5da7e9a75744c15
lxdi <alexfordeal@gmail.com>
2021-01-29 22:51:17 +0300

    init: compiled and running

--- frontend/source/app.js
+++ frontend/source/app.js
@@ -3,31 +3,7 @@ import ReactDOM from 'react-dom';
 
 import {Main} from './ui/main'
 import {LeftSideBarContent} from './ui/left-side-bar-content'
-import {TaskProgressModal} from './ui/means/modal/task-progress-modal'
-import {TaskFinishModal} from './ui/means/modal/task-finish-modal'
-import {ActualTasksModal} from './ui/means/actual-tasks-modal'
-import {DayModal} from './ui/schedule/day-modal'
-import {ShiftPlansModal} from './ui/schedule/shift-plans-modal'
-import {ExternalTaskModal} from './ui/schedule/external-task-modal'
 import {OverlayInfo} from './ui/overlay'
-
-import {createRealmRep} from './data/realms-dao'
-import {createTargetRep} from './data/targets-dao'
-import {createMeanRep} from './data/means-dao'
-import {createLayerRep} from './data/layers-dao'
-import {createTaskRep} from './data/tasks-topics-testings-dao'
-
-import './data/rep-plans-dao'
-import './data/weeks-dao'
-import './data/days-dao'
-import './data/progress-dao'
-import './data/external-tasks-dao'
-
-createRealmRep()
-createTargetRep()
-createMeanRep()
-createLayerRep()
-createTaskRep()
 
 ReactDOM.render(<div id="app" />, document.body);
 const app = document.getElementById("app");
@@ -35,23 +11,10 @@ const app = document.getElementById("app");
 function rerender(){
 	ReactDOM.render(
 		<div style={{margin:'3px'}}>
-				<TaskProgressModal/>
-				<TaskFinishModal/>
-				<ActualTasksModal/>
-				<DayModal/>
-				<ShiftPlansModal/>
-				<ExternalTaskModal/>
 				<OverlayInfo />
-				<table style={{width:'100%'}}>
-					<tr>
-						<td style={{width:'50px', verticalAlign:'top'}}>
-							<LeftSideBarContent/>
-						</td>
-						<td>
-							<Main />
-						</td>
-					</tr>
-				</table>
+				<div>
+					<Main />
+				</div>
 		</div>, app);
 }
 
--- frontend/source/ui/left-side-bar-content.js
+++ frontend/source/ui/left-side-bar-content.js
@@ -5,18 +5,10 @@ import {registerEvent, registerReaction, fireEvent, chkSt} from 'absevents'
 
 import {DataConstants} from '../data/data-constants'
 import {filterOutMemoTask} from '../utils/task-utils'
-import {getColorForProgressStat, getPercents} from './statService'
-
-const taskRep = 'task-rep'
 
 export class LeftSideBarContent extends React.Component {
   constructor(props){
     super(props)
-
-    registerReaction('actual-tasks-ui', taskRep, ['actual-tasks-rs'], ()=>this.setState({}))
-    registerReaction('actual-tasks-ui', 'main-ui', ['switch-mode'], ()=>this.setState({}))
-
-    registerReaction('actual-tasks-ui', DataConstants.progressRep, ['got-actual'], (stateSetter)=>this.setState({}))
   }
 
   render(){
@@ -26,32 +18,17 @@ export class LeftSideBarContent extends React.Component {
 }
 
 const content = function(reactcomp){
-  const actuals = chkSt(DataConstants.progressRep, 'actual');
-  if(actuals==null){
-    fireEvent(DataConstants.progressRep, 'get-actual')
-    return 'Loading...'
-  }
   return <div>
               <a href="#" style={{textDecoration:'none'}}>
                 <div onClick={()=>fireEvent('actual-tasks-modal', 'open')} class="actual-tasks-indicators-group">
-                  {getSquare(actuals.twoWeeksLate.length, 'red')}
-                  {getSquare(actuals.oneWeekLate.length, 'orange')}
-                  {getSquare(actuals.aboutWeekReps.length, 'green')}
-                  {getSquare(actuals.upcomingReps.length, 'grey')}
-                  {divisor()}
-                  {getStatSquare(actuals.weekProgress, getPercents(actuals.weekProgress, 'week'), getColorForProgressStat(actuals.weekProgress, 'week'), 'Week')}
-                  {getStatSquare(actuals.monthProgress, getPercents(actuals.monthProgress, 'month'), getColorForProgressStat(actuals.monthProgress, 'month'), 'Month')}
-                  {getStatSquare(actuals.halfYearProgress, getPercents(actuals.halfYearProgress, 'halfYear'), getColorForProgressStat(actuals.halfYearProgress, 'halfYear'), 'Year/2')}
-                  {getStatSquare(actuals.yearProgress, getPercents(actuals.yearProgress, 'year'), getColorForProgressStat(actuals.yearProgress, 'year'), 'Year')}
+                  {getSquare(14, 'red')}
+                  {getSquare(15, 'red')}
                 </div>
               </a>
               {divisor()}
               <div style={{margin:'2px', width:'45px', height:'45px', fontSize:'10pt'}}>
-                <button class="left-bar-button" onClick = {()=>fireEvent('main-ui', 'switch-mode')}>
-                {viewModeButtonLabel()}
-                </button>
+
               </div>
-              {getSwitchButton()}
             </div>
 }
 
@@ -69,26 +46,4 @@ const getStatSquare = function(num, percents, color, span){
                   <div style = {{fontSize:'8pt'}}>{num}</div>
                   <div style = {{fontSize:'8pt'}}>{percents}%</div>
             </div>
-}
-
-const getSwitchButton = function(){
-  if(!chkSt('main-ui', 'three-frames')){
-    return <div style={{margin:'2px', width:'45px', height:'45px'}}>
-                    <button class="left-bar-button-switch" style={{fontSize:'8pt'}} onClick={()=>fireEvent('main-ui', 'switch-curr-state')}>Switch</button>
-                  </div>
-  } else {
-    return <div></div>
-  }
-}
-
-const viewModeButtonLabel = function(){
-  if(!chkSt('main-ui', 'three-frames')){
-    return ' |  | '
-  } else {
-    return <div>
-                 <span style={{color:'green'}}> | </span>
-                 <span style={{color:'blue'}}> | </span>
-                 <span style={{color:'orange'}}> | </span>
-            </div>
-  }
 }
